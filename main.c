4097d8fea38a25d60da994693d452528570073e1
Nermirc <141329241+Nermirc@users.noreply.github.com>
2023-10-24 21:37:12 +0300

    Area of square
    calculating area of square with float and int

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   /* int x,alan;
+    printf("Karenin kenar uzunlugunu giriniz.\n");
+    scanf("%d",&x);
+    alan=x*x;
+    printf("Alan=%d",alan);
+    // With float
+    float y,alan;
+    printf("Karenin kenar uzunlugunu giriniz.\n");
+    scanf("%f",&y);
+    alan=y*y;
+    printf("Alan = %f",alan);  */
+
+}
